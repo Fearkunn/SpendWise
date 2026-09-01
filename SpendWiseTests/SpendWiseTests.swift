5a6834a1f5757ab3e83400bed769aa1fb3e666dc
Fearkunn <richiedaryl@gmail.com>
2026-09-01 16:56:42 +0700

    initial setup

---
+++ SpendWiseTests/SpendWiseTests.swift
@@ -0,0 +1,19 @@
+//
+//  SpendWiseTests.swift
+//  SpendWiseTests
+//
+//  Created by Richie Daryl Kwenandar on 01/09/26.
+//
+
+import Testing
+@testable import SpendWise
+
+struct SpendWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
